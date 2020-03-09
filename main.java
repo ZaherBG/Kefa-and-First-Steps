ed60f279814820f194cc7bc4f7164cc1d91465af
ZaherBG <59502225+ZaherBG@users.noreply.github.com>
2020-03-09 02:27:16 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Kefa-and-First-Steps {
+    public static void main(String[] args) {
+             int m;
+        Scanner s = new Scanner(System.in);
+        m= s.nextInt();
+        int [] p = new int[m];
+         for(int i=0;i<p.length;i++){
+             p[i]= s.nextInt();
+         }
+        System.out.println(count(p));
+    }
+    public static int count(int [] a){
+     int b=1, c=1;
+          for(int i=0;i<a.length-1;i++){
+         if(a[i+1]>=a[i]){
+             b++;
+         }
+         else{
+             c= Math.max(c, b);
+             b=1;
+         }
+             c= Math.max(c, b); 
+    }
+          return c;
+}
+    }
